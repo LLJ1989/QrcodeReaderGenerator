56f525d905fcdd2675a456a2a93b2659356acbd7
Lucas <l.lucaslombard@free.fr>
2020-08-01 23:19:09 +0200

    7 - SHARING - add sharing possibility when qr-code is already created

--- QrcodeReaderGenerator/Controller/MakerResultViewController.swift
+++ QrcodeReaderGenerator/Controller/MakerResultViewController.swift
@@ -16,6 +16,7 @@ class MakerResultViewController: UIViewController {
   // MARK: - Outlets
   @IBOutlet weak var qrcodeIV: UIImageView!
   @IBOutlet weak var dismissBTN: CustomUIButton!
+  @IBOutlet weak var actionBTN: UIButton!
 
   // MARK: - Methods
   override func viewDidLoad() {
@@ -27,5 +28,37 @@ class MakerResultViewController: UIViewController {
   // MARK: - Action
   @IBAction func didTapDismissBTN(_ sender: CustomUIButton) {
     dismiss(animated: true)
+  }
+}
+
+// MARK: - This extension manage // SHARING \\
+extension MakerResultViewController {
+
+  // MARK: - Action
+  @IBAction func didTapActionBTN(_ sender: UIButton) {
+    askUser()
+  }
+
+  // MARK: - Methods
+  /// This methood ask user
+  private func askUser() {
+    let sheet = UIAlertController(title: nil,
+                                  message: "Que souhaitez-vous faire ?",
+                                  preferredStyle: .actionSheet)
+    sheet.addAction(UIAlertAction(title: "Annuler", style: .default, handler: nil))
+    sheet.addAction(UIAlertAction(title: "Partager l'image", style: .default, handler: { (_) in
+      self.shareImage()
+    }))
+    present(sheet, animated: true)
+  }
+
+  /// This method share image
+  private func shareImage() {
+    guard let image = qrcodeIV.image else { return }
+    let imageToShare = [image]
+    let activityViewController = UIActivityViewController(activityItems: imageToShare, applicationActivities: nil)
+    activityViewController.popoverPresentationController?.sourceView = self.view
+    activityViewController.excludedActivityTypes = [.postToVimeo]
+    self.present(activityViewController, animated: true)
   }
 }
